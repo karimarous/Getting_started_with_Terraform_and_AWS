dd973059d4d332aeb2d14814a13ed5900438d9c7
karimarous <karimarous@gmail.com>
2025-05-23 15:09:54 +0200

    Add labs v1

--- lab20/modules/ec2/main.tf
+++ lab20/modules/ec2/main.tf
@@ -45,6 +45,6 @@ resource "aws_instance" "instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   tags = {
-    Name = "var.instance_name-${var.env}"
+    Name = "${var.instance_name}-${var.env}"
   }
 }
